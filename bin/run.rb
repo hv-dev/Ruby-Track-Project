9cda670b955c2b7eee7d989c65c04c7d37f69f2f
Harry Verrall <harryv@chillipharm.com>
2022-09-22 15:45:49 +0100

    Initial Version [No Tests]
    Initial version of the completed version of the ticket.
    
    No tests have been written yet but rspec --init has been run
    
    next commit will contain any needed configuration changes and tests

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,49 @@
 require_relative '../config/environment'
 
+CLEAR = "\e[H\e[2J"
 
+if TTY::Prompt.new().yes?("Would you like to start the program?")
+    print CLEAR
 
-puts "HELLO WORLD"
+=begin
+    box = TTY::Box.frame(
+        width: 167, height: 40,
+        border: :thick, padding: 1,
+        align: :center,
+        title: {top_left: "Terminal Pokédex", bottom_right: "CC-29"}) do
+        "Test Message that should appear on screen within the box"
+    end
+=end
+
+    prompt = TTY::Prompt.new()
+
+    guest_menu_options = {
+        "Login" => "login",
+        "Create an account" => "create_account",
+        "Pokédex" => "dex",
+        "Rivals" => "rivals",
+        "Close Program" => "close_program"  
+    }
+
+    selected_option = prompt.select("Please select an option", guest_menu_options, marker: ">>", required: true, filter: true)
+
+    case selected_option
+        when "login"
+            puts "Logging in"
+        when "create_account"
+            puts "Creating an account"
+        when "dex"
+            puts "Opening Pokédex"
+        when "rivals"
+            puts "Searching for Rvials"
+        when "close_program"
+            quit_program
+        else
+            puts "You have managed to select an invalid option, closing program"
+    end
+else
+    print CLEAR
+    abort
+end
+
+#puts box
